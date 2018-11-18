f9528bc8c504e9117aec00a9dda15e2fb62d0f46
yushao <yushao@ebay.com>
2018-11-18 16:45:31 +0800

    Test Rebase10.1

--- src/MaxXor.java
+++ src/MaxXor.java
@@ -8,7 +8,8 @@
 * Test Rebase3
 * Test Rebase4
 * Test Rebase5
-* Test Rebase10*/
+* Test Rebase10
+* Test Rebase10.1*/
 public class MaxXor {
 	static MaxXor mx = new MaxXor();
 	private static final int N = 100002;
